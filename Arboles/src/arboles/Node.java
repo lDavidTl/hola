08e0feab8c728320ab8060aef9a04d8ce33364a1
Liseth-G <gg18112@ues.edu.sv>
2020-11-24 16:53:45 -0600

    m

--- Arboles/src/arboles/Node.java
+++ Arboles/src/arboles/Node.java
@@ -75,23 +75,26 @@ public class Node {
 
 }
 
- public void printInOrder() throws SQLException {
+ public void printInOrder(int indice) throws SQLException {
      if (Izdo != null) {
-     Izdo.printInOrder();
+       //  indice++; //----------
+     Izdo.printInOrder(indice++);
      }
      System.out.println(valor);
  //------- guadar en la bd en inOrden----------
      ConexionSQL con = new ConexionSQL();
      con.conector();
      String preOrden = String.valueOf(valor);
-     String SQL = "INSERT INTO tabla (inOrden)VALUES (?)";
+     //String SQL = "INSERT INTO tabla (inOrden)VALUES (?)";
+     String SQL = "UPDATE tabla SET inOrden= ? WHERE id = ?"; //--------
      PreparedStatement pst = con.prepareStatement(SQL);
      pst.setString(1,preOrden);
+     pst.setInt(2,indice);
      pst.execute();
   //---------------------------------------------
  if (Dcho != null) {
-     
- Dcho.printInOrder();
+     //indice++;//-------
+ Dcho.printInOrder(indice++);
 
 }
 
@@ -124,25 +127,27 @@ public class Node {
 
  
 }
- public void printPosOrder() throws SQLException {
+ public void printPosOrder(int indice) throws SQLException {
      
  if (Izdo != null) {
-     
- Izdo.printPosOrder();
+     indice++;
+ Izdo.printPosOrder(indice++);
 
 }
  if (Dcho != null) {
-     
- Dcho.printPosOrder();
+     indice++;
+ Dcho.printPosOrder(indice++);
 
 }
  //-------guardar en la d en posorden----------
  ConexionSQL con = new ConexionSQL();
      con.conector();
      String preOrden = String.valueOf(valor);
-  String SQL = "INSERT INTO tabla (posOrden)VALUES (?)";
+  //String SQL = "INSERT INTO tabla (posOrden)VALUES (?)";
+  String SQL = "UPDATE tabla SET posOrden= ? WHERE id = ?"; //--------
   PreparedStatement pst = con.prepareStatement(SQL);
   pst.setString(1,preOrden);
+  pst.setInt(2,indice);
   pst.execute();
  System.out.println(valor);
 }
